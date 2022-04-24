09a64ca7d109e6e6c893ffaa0e87d0bc6f2f10d8
Phil Smith <philip.smith.k@gmail.com>
2022-04-24 15:55:59 -0400

    Initial logic for flag calculation and initial components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function FlagResult(props) {
+
+    const isActive = (props.flags & props.base) > 0;
+
+    return (
+        <li>
+            {isActive.toString()}
+        </li>
+    )
+}
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    const handleChange = (e) => {
+        const flags = e.target.value;
+        setFlags(flags);
+    }
+  
+    const [flags, setFlags] = useState(0);
+
+    return (
+        <div>
+            <input onChange={handleChange} type="number" min="0"></input>
+            <ul>
+                <FlagResult base={0} flags={flags}></FlagResult> 
+                <FlagResult base={1} flags={flags}></FlagResult> 
+                <FlagResult base={2} flags={flags}></FlagResult> 
+                <FlagResult base={4} flags={flags}></FlagResult>
+                <FlagResult base={8} flags={flags}></FlagResult>
+            </ul>
+        </div>
   );
 }
 
